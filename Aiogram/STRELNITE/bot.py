33c2f1e4c7a62fb8cef4e2385e95705c64a08cf4
DightMerc <dightmerc@gmail.com>
2018-10-14 23:22:18 +0500

    Strelnite bot starting

---
+++ Aiogram/STRELNITE/bot.py
@@ -0,0 +1,33 @@
+import asyncio
+import logging
+
+from aiogram import Bot, types, Dispatcher, executor
+
+API_TOKEN = '679147382:AAGRMZ1t2wrGuYBQOMg-jhNiWnWNUUOkDUM'
+
+logging.basicConfig(level=logging.INFO)
+
+loop = asyncio.get_event_loop()
+bot = Bot(token=API_TOKEN, loop=loop)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler(regexp='(^cat[s]?$|puss)')
+async def cats(message: types.Message):
+    with open('data/cats.jpg', 'rb') as photo:
+        await bot.send_photo(message.chat.id, photo, caption='Cats is here 😺',
+                             reply_to_message_id=message.message_id)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, loop=loop, skip_updates=True)
